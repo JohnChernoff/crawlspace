31977320884bc582874cdc1b28a04d21a61349dd
John Chernoff <jachernoff@gmail.com>
2026-03-28 20:14:32 -0700

    reg changes

--- engine/lib/galaxy/reg/reg.dart
+++ engine/lib/galaxy/reg/reg.dart
@@ -6,12 +6,8 @@ import 'package:crawlspace_engine/galaxy/reg/pilot_reg.dart';
 import 'package:crawlspace_engine/galaxy/reg/plan_reg.dart';
 import 'package:crawlspace_engine/galaxy/reg/ship_reg.dart';
 import 'package:crawlspace_engine/galaxy/reg/star_reg.dart';
-import 'package:crawlspace_engine/ship/nav.dart';
-import '../../actors/pilot.dart';
 import '../../item.dart';
-import '../../ship/ship.dart';
 import '../geometry/location.dart';
-import '../geometry/object.dart';
 import '../system.dart';
 import 'item_reg.dart';
 import 'locatables.dart';
@@ -25,19 +21,39 @@ class RegModel extends GalaxySubMod {
   RegModel(super.galaxy);
 }
 
-typedef ItemMap = Map<SpaceLocation,Set<Item>>;
+typedef ItemMap = Map<SpaceLocation, Set<Item>>;
 typedef ItemEntry = MapEntry<SpaceLocation, Set<Item>>;
 typedef ItemSet = Set<ItemEntry>;
 
+enum OccupancyPolicy {
+  single,
+  multiple,
+}
+
 abstract class SpaceRegistry<T extends Locatable<L>, L extends SpaceLocation> {
+  //static const _empty = <T>{}; // won't work directly because T is generic
   final Map<T, L> _locations = {};
   final Map<System, Set<T>> _bySystem = {};
+  T? _singleOrNull(Set<T> bucket, Object key) {
+    if (bucket.isEmpty) return null;
+    if (bucket.length > 1) {
+      throw StateError(
+        'Expected single $T at $key, found ${bucket.length}',
+      );
+    }
+    return bucket.first;
+  }
+
+  OccupancyPolicy get occupancyPolicy => OccupancyPolicy.multiple;
 
   void register(T obj, L loc) {
-    assert(!_locations.containsKey(obj), '$obj is already registered at ${_locations[obj]}',);
+    assert(
+    !_locations.containsKey(obj),
+    '$obj is already registered at ${_locations[obj]}',
+    );
     obj.onRegistered(loc);
     _locations[obj] = loc;
-    _bySystem.putIfAbsent(loc.system, () => {}).add(obj);
+    _bySystem.putIfAbsent(loc.system, () => <T>{}).add(obj);
     onRegister(obj, loc);
   }
 
@@ -46,7 +62,7 @@ abstract class SpaceRegistry<T extends Locatable<L>, L extends SpaceLocation> {
     register(obj, newLoc);
   }
 
-  L remove(T obj, {moving = false}) {
+  L remove(T obj, {bool moving = false}) {
     final old = _locations[obj];
     if (old == null) {
       throw StateError('$obj is not registered');
@@ -61,148 +77,150 @@ abstract class SpaceRegistry<T extends Locatable<L>, L extends SpaceLocation> {
   }
 
   L? locationOf(T obj) => _locations[obj];
-  Set<T> inSystem(System s) => _bySystem[s] ?? {};
+  Set<T> inSystem(System s) => _bySystem[s] ?? <T>{};
 
   void onRegister(T obj, L loc) {}
   void onRemove(T obj, L loc) {}
 }
 
-abstract class _SectorIndexedRegistry<T extends Locatable<L>, L extends SystemLocation> extends SpaceRegistry<T, L> {
+abstract class _SectorIndexedRegistry<T extends Locatable<L>, L extends SystemLocation>
+    extends SpaceRegistry<T, L> {
   final Map<SectorLocation, Set<T>> _bySector = {};
-  Set<T> inSector(SectorLocation s) => _bySector[s] ?? {};
+  Set<T> inSector(SectorLocation s) => _bySector[s] ?? <T>{};
+  T? singleAtSector(SectorLocation loc) => _singleOrNull(inSector(loc), loc);
 
-  @override
-  void register(T obj, L loc) {
-    super.register(obj, loc);
-    _bySector.putIfAbsent(
-      SectorLocation(loc.system, loc.sectorCoord),
-          () => {},
-    ).add(obj);
+  void _checkBucketOccupancy(Set<T> bucket, Object key) {
+    if (occupancyPolicy == OccupancyPolicy.single && bucket.isNotEmpty) {
+      throw StateError('$T ${bucket.first} already at $key');
+    }
   }
 
   @override
-  L remove(T obj, {moving = false}) {
+  void register(T obj, L loc) {
+    final key = SectorLocation(loc.system, loc.sectorCoord);
+    final bucket = _bySector.putIfAbsent(key, () => <T>{});
+    _checkBucketOccupancy(bucket, key);
+    super.register(obj, loc);
+    bucket.add(obj);
+  }
+
+  @override
+  L remove(T obj, {bool moving = false}) {
     final old = super.remove(obj, moving: moving);
-    _bySector[SectorLocation(old.system, old.sectorCoord)]?.remove(obj);
+    final key = SectorLocation(old.system, old.sectorCoord);
+    final bucket = _bySector[key];
+    if (bucket == null || !bucket.remove(obj)) {
+      throw StateError('Unexpected registry state for $obj at $key');
+    }
+    if (bucket.isEmpty) _bySector.remove(key);
     return old;
   }
 }
 
 abstract class SectorRegistry<T extends Locatable<SectorLocation>>
-    extends _SectorIndexedRegistry<T, SectorLocation> {
-}
+    extends _SectorIndexedRegistry<T, SectorLocation> {}
 
 abstract class _ImpulseIndexedRegistry<T extends Locatable<L>, L extends ImpulseScope>
     extends _SectorIndexedRegistry<T, L> {
-  final Map<ImpulseLocation, T> _byImpulse = {};
-
-  T? byImpulse(ImpulseLocation loc) => _byImpulse[loc];
+  final Map<ImpulseLocation, Set<T>> _byImpulse = {};
+  Set<T> inImpulse(ImpulseLocation loc) => _byImpulse[loc] ?? <T>{};
+  T? singleAtImpulse(ImpulseLocation loc) => _singleOrNull(inImpulse(loc), loc);
 
   @override
   void register(T obj, L loc) {
     final key = ImpulseLocation(loc.system, loc.sectorCoord, loc.impulseCoord);
-    assert(!_byImpulse.containsKey(key), '$T ${_byImpulse[key]} already at $key');
+    final bucket = _byImpulse.putIfAbsent(key, () => <T>{});
+    _checkBucketOccupancy(bucket, key);
     super.register(obj, loc);
-    _byImpulse[key] = obj;
+    bucket.add(obj);
   }
 
   @override
-  L remove(T obj, {moving = false}) {
+  L remove(T obj, {bool moving = false}) {
     final old = super.remove(obj, moving: moving);
     final key = ImpulseLocation(old.system, old.sectorCoord, old.impulseCoord);
-    if (!identical(_byImpulse[key], obj)) {
+    final bucket = _byImpulse[key];
+    if (bucket == null || !bucket.remove(obj)) {
       throw StateError('Unexpected registry state for $obj at $key');
     }
-    _byImpulse.remove(key);
+    if (bucket.isEmpty) _byImpulse.remove(key);
     return old;
   }
 }
 
 abstract class ImpulseRegistry<T extends Locatable<ImpulseLocation>>
     extends _ImpulseIndexedRegistry<T, ImpulseLocation> {
+  @override
+  OccupancyPolicy get occupancyPolicy => OccupancyPolicy.single;
 
-  Coord3D randomEmptyCoord(System system, Coord3D sectorCoord, GridDim dim, Random rnd) {
-    var c;
+  Coord3D randomEmptyCoord(
+      System system,
+      Coord3D sectorCoord,
+      GridDim dim,
+      Random rnd,
+      ) {
+    Coord3D c;
     do {
       c = dim.rndCoord(rnd);
-    }
-    while (byImpulse(ImpulseLocation(system, sectorCoord, c)) != null);
+    } while (inImpulse(ImpulseLocation(system, sectorCoord, c)).isNotEmpty);
     return c;
   }
 }
 
 abstract class _OrbitalIndexedRegistry<T extends Locatable<L>, L extends OrbitalLocation>
     extends _ImpulseIndexedRegistry<T, L> {
-  final Map<OrbitalLocation, T> _byOrbital = {};
-
-  T? byOrbital(OrbitalLocation loc) => _byOrbital[loc];
+  final Map<OrbitalLocation, Set<T>> _byOrbital = {};
+  Set<T> inOrbital(OrbitalLocation loc) => _byOrbital[loc] ?? <T>{};
+  T? singleAtOrbital(OrbitalLocation loc) => _singleOrNull(inOrbital(loc), loc);
 
   @override
   void register(T obj, L loc) {
-    final key = OrbitalLocation(loc.system, loc.sectorCoord, loc.impulseCoord, loc.orbitalCoord);
-    assert(!_byOrbital.containsKey(key), '$T ${_byOrbital[key]} already at $key');
+    final key = OrbitalLocation(
+      loc.system,
+      loc.sectorCoord,
+      loc.impulseCoord,
+      loc.orbitalCoord,
+    );
+    final bucket = _byOrbital.putIfAbsent(key, () => <T>{});
+    _checkBucketOccupancy(bucket, key);
     super.register(obj, loc);
-    _byOrbital[key] = obj;
+    bucket.add(obj);
   }
 
   @override
-  L remove(T obj, {moving = false}) {
+  L remove(T obj, {bool moving = false}) {
     final old = super.remove(obj, moving: moving);
-    final key = OrbitalLocation(old.system, old.sectorCoord, old.impulseCoord, old.orbitalCoord);
-    if (!identical(_byOrbital[key], obj)) {
+    final key = OrbitalLocation(
+      old.system,
+      old.sectorCoord,
+      old.impulseCoord,
+      old.orbitalCoord,
+    );
+    final bucket = _byOrbital[key];
+    if (bucket == null || !bucket.remove(obj)) {
       throw StateError('Unexpected registry state for $obj at $key');
     }
-    _byOrbital.remove(key);
+    if (bucket.isEmpty) _byOrbital.remove(key);
     return old;
   }
 }
 
 abstract class OrbitalRegistry<T extends Locatable<OrbitalLocation>>
     extends _OrbitalIndexedRegistry<T, OrbitalLocation> {
-  Coord3D randomEmptyCoord(System system, Coord3D sectorCoord, Coord3D impCoord, GridDim dim, Random rnd) {
-    var c;
+  @override
+  OccupancyPolicy get occupancyPolicy => OccupancyPolicy.single;
+
+  Coord3D randomEmptyCoord(
+      System system,
+      Coord3D sectorCoord,
+      Coord3D impCoord,
+      GridDim dim,
+      Random rnd,
+      ) {
+    Coord3D c;
     do {
       c = dim.rndCoord(rnd);
-    }
-    while (byOrbital(OrbitalLocation(system, sectorCoord, impCoord, c)) != null);
+    } while (inOrbital(OrbitalLocation(system, sectorCoord, impCoord, c)).isNotEmpty);
     return c;
   }
 }
-
-
-
-/*
-abstract class ImpulseRegistryz<T extends Locatable<ImpulseLocation>>
-    extends _SectorIndexedRegistry<T, ImpulseLocation> {
-  final Map<ImpulseLocation, T> _byImpulse = {};
-
-  T? byImpulse(ImpulseLocation loc) => _byImpulse[loc];
-
-  @override
-  void register(T obj, ImpulseLocation loc) {
-    assert(!_byImpulse.containsKey(loc),
-    '$T ${_byImpulse[loc]} already at $loc');
-    super.register(obj, loc);
-    _byImpulse[loc] = obj;
-  }
-
-  @override
-  ImpulseLocation remove(T obj, {moving = false}) {
-    final old = super.remove(obj, moving: moving);
-    if (!identical(_byImpulse[old], obj)) {
-      throw StateError('Unexpected registry state for $obj at $old');
-    }
-    _byImpulse.remove(old);
-    return old;
-  }
-
-  Coord3D randomEmptyCoord(System system, Coord3D sectorCoord, GridDim dim, Random rnd) {
-    var c;
-    do {
-      c = dim.rndCoord(rnd);
-    }
-    while (byImpulse(ImpulseLocation(system, sectorCoord, c)) != null);
-    return c;
-  }
-}
- */
