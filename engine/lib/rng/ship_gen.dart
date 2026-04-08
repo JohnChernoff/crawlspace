1741afcf4f5af29c77821d741ace8de8bdc04637
John Chernoff <jachernoff@gmail.com>
2026-04-08 13:32:31 -0700

    shipgen bugfix

--- engine/lib/rng/ship_gen.dart
+++ engine/lib/rng/ship_gen.dart
@@ -12,29 +12,33 @@ import '../stock_items/ship/stock_ships.dart';
 
 class ShipGenerator {
   static Ship generateShip(System system, Galaxy galaxy, Random rnd, {required Pilot owner}) {
+    //print("Generating ship...");
     final location = SectorLocation(system, system.map.rndCoord(rnd)); //galaxy.rndLoc(rnd);
     final level = max(0,1 - (galaxy.topo.distance(location.system, galaxy.findHomeworld(owner.faction.species)) / galaxy.maxJumps));
     final techLvl = max(1,(level * 10).round()); //TODO: something more sophisticated?
     //print("Faction: ${pilot.faction.name}, tech: $level, $techLvl");
     ShipType shipType = Rng.weightedRandom(owner.faction.shipWeights.normalized,rnd);
     bool okType(ShipType type) {
-      if (level > shipType.dangerLvl) return false;
+      if (shipType.dangerLvl > level) return false;
       if (owner.faction.isPirate || owner.faction.isWarmonger) {
         return rnd.nextDouble() < .1 || type.slots.map((s) => s.type).contains(ShipSystemType.weapon);
       }
       return true;
     }
-    while (okType(shipType)) {
+    int guard = 0;
+    while (!okType(shipType)) {
+      if (++guard > 1000) {
+        throw StateError("Ship generation failed to find a valid ship type for ${owner.faction.name}");
+      }
       if (rnd.nextDouble() < shipType.dangerLvl) {
         shipType = ShipType.values.elementAt(rnd.nextInt(ShipType.values.length));
       } else {
         shipType = Rng.weightedRandom(owner.faction.shipWeights.normalized,rnd);
       }
     }
-    //print("Ship Type: $shipType");
     final shipClassType = ShipClassType.values.firstWhereOrNull((t) => t.type == shipType) ?? ShipClassType.mentok;
-    //print("Ship Type: $shipClassType");
     Ship ship = Ship("HMS ${Rng.randomAlienName(rnd)}", shipClass: ShipClass.fromEnum(shipClassType), techLvl: techLvl, owner: owner);
+    //print("Generated ship: $ship");
     return ship;
   }
 
